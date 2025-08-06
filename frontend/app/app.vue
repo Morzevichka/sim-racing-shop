b681aed6638aac5dfea1305c9a0dcf923a68742a
Igor Solovev <solovjev.igor.2005@gmail.com>
2025-08-06 14:06:32 +0300

    init setup nuxt and create header

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,3 +1,30 @@
+<script setup>
+useHead({
+  htmlAttrs: {
+    lang: 'ru'
+  },
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+    { charset: 'utf-8' }
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+  ]
+})
+
+useSeoMeta({
+  title: 'Sim Racing Shop',
+  ogTitle: 'Sim Racing Shop',
+  description: 'Site for buying sim racing equipment.',
+  ogDescription: 'Site for buying sim racing equipment.',
+  ogType: 'website',
+  ogLocale: 'ru_RU',
+  twitterCard: 'summary_large_image'
+})
+</script>
+
 <template>
-  hello
+  <Header />
+    <NuxtPage />
+  <Footer />
 </template>
