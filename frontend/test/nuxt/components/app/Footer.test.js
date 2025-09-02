e173ecd708217d1ecc6a5146dc279ebcd01ca58c
Igor Solovev <solovjev.igor.2005@gmail.com>
2025-09-02 22:48:45 +0300

    test(frontend): Add test to Footer.vue component

---
+++ frontend/test/nuxt/components/app/Footer.test.js
@@ -0,0 +1,37 @@
+import { mountSuspended } from '@nuxt/test-utils/runtime'
+import { it, expect, describe, beforeAll } from 'vitest'
+import Footer from '~/components/app/Footer.vue'
+
+describe('Footer.vue (unit)', () => {
+    let component = null
+
+    beforeAll(async () => {
+        component = await mountSuspended(Footer)
+    })
+
+    it('can mount the component', async () => {
+        expect(component.find('#footer-socials').exists()).toBe(true)
+        expect(component.findAll('.footer-container').length).toBe(3)
+        expect(component.find('#footer-bottom-wrapper').exists()).toBe(true)
+        expect(component.findAll('a').length).toBe(16)
+    })
+
+    it('contains right links to socials', async () => {
+        const socialsLinks = component.findAll('#footer-socials a').map(l => l.attributes('href'))
+
+        expect(socialsLinks).toContain('https://vk.com')
+        expect(socialsLinks).toContain('https://instagram.com')
+        expect(socialsLinks).toContain('https://youtube.com')
+        expect(socialsLinks).toContain('https://telegram.com')
+    })
+
+    it('contains right links in Navigation and Products', async () => {
+        component = await mountSuspended(Footer)
+        const links = component.findAll('.footer-container a').map(l => l.attributes('href'))
+
+        expect(links).toContain('/')
+        expect(links).toContain('/catalog')
+        expect(links).toContain('/about')
+        expect(links).toContain('/qa')
+    })
+})
